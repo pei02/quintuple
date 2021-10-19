ed6ea552072d62242946c595aa81b835157d0c37
pei02 <57085184+pei02@users.noreply.github.com>
2021-10-19 14:51:11 +0800

    bug修正

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -16,7 +16,7 @@ export class ResultComponent implements OnInit {
   result: any = [];
   show: number = 0;
   font_color: any = [];
-  two_num:any='';
+  lottery_num:any='';
 
   constructor() {
 
@@ -45,15 +45,17 @@ export class ResultComponent implements OnInit {
 
   search() {
     this.result = [];
-    this.two_num=this.num.length>=3?this.num.slice(1,3):this.num;
+    this.lottery_num=this.num.length<3?"*"+this.num:this.num;
+    console.log(this.lottery_num);
     var _data = this.quontupleList.filter(data => data.week == this.select_value);
     _data.forEach(data => {
       data.class.forEach(vouchers => {
-        if (vouchers.number.indexOf(this.num) != -1 || vouchers.number.indexOf(this.two_num) != -1) {
+        if (vouchers.number.indexOf(this.num) != -1 || vouchers.number.indexOf(this.num.slice(1,3)) != -1) {
           this.result.push(vouchers);
         }
       })
     })
     this.show = this.result.length != 0 ? 1 : 2;
   }
+
 }
