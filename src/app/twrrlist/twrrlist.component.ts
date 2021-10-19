84ee0205b6edbd21f30b4591934ec7b1fc650c27
pei02 <57085184+pei02@users.noreply.github.com>
2021-10-19 19:19:52 +0800

    debug

--- src/app/twrrlist/twrrlist.component.ts
+++ src/app/twrrlist/twrrlist.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {twrrList} from './twrrlist.const'
+import { twrrList } from './twrrlist.const'
 
 @Component({
   selector: 'app-twrrlist',
@@ -7,33 +7,38 @@ import {twrrList} from './twrrlist.const'
   styleUrls: ['./twrrlist.component.scss']
 })
 export class TwrrlistComponent implements OnInit {
-  
-  twrrList=twrrList;
-  cityList:any=[];
-  select_city:any="所有縣市";
-  search_storyname:any="";
-  result:any=[];
+
+  twrrList = twrrList;
+  cityList: any = [];
+  select_city: any = "所有縣市";
+  search_storyname: any = "";
+  result: any = [];
 
   constructor() { }
 
   ngOnInit(): void {
-   this.result=this.twrrList;
-   this.city_List_Array();
+    this.result = this.twrrList;
+    this.city_List_Array();
   }
 
-  city_List_Array(){
-     let _city=new Array();
-    this.twrrList.forEach(x=>{ 
+  city_List_Array() {
+    let _city = new Array();
+    this.twrrList.forEach(x => {
       _city.push(x.city);
     })
-    this.cityList=[...new Set(_city)];
+    this.cityList = [...new Set(_city)];
   }
 
-  search(){
-    this.result=this.twrrList.filter(search=>search.city==this.select_city&&search.store.indexOf(this.search_storyname)>-1);
+  search() {
+    if (this.select_city == "所有縣市") {
+      let _result = this.twrrList.filter(search => search.store.indexOf(this.search_storyname) > -1)
+      this.result = _result;
+    } else {
+      this.result = this.twrrList.filter(search => search.city == this.select_city && search.store.indexOf(this.search_storyname) > -1);
+    }
   }
- 
-  toTop(){
+
+  toTop() {
     window.scrollTo(0, 0);
   }
 
