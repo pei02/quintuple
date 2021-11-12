d8e157e89ad4a3203587e4cd719919a6e21fa603
pei02 <57085184+pei02@users.noreply.github.com>
2021-11-12 17:28:06 +0800

    地方創生券商家更新

--- src/app/level/level.component.ts
+++ src/app/level/level.component.ts
@@ -10,12 +10,12 @@ import { quontupleList } from '../result/quintuplelist.const';
 export class LevelComponent implements OnInit {
   quontupleList = quontupleList;
   result:any=[];
-  lucky_number:any;
+  lucky_number:any=[];
   constructor() { }
 
   ngOnInit(): void {
     this.lottery_Array();
-this.lucky();
+ 
   }
 
   lottery_Array(){
@@ -27,21 +27,30 @@ this.lucky();
         })
      })
     })
-    this.result=[...new Set(lottery_array)];
-    console.log(this.result.sort())
+    this.result=[...new Set(lottery_array)].sort();
+    console.log(this.result)
   }
 
-  lucky(){
-   
-      for(let i=0;i<this.result.length;i++){
-        this.quontupleList.forEach(x=>{
-          x.class.filter(y=>{
-            console.log(y.number.indexOf(this.result[i])>-1)
-          })
-        })
-      }
-
-  }
+//   lucky(){
+      
+//       for(let i=0;i<this.result.length;i++){
+//         let seconed=0;
+//         this.quontupleList.forEach(x=>{
+//           x.class.filter(y=>{ 
+//             let two_number=this.result[i].length>2?;
+//             console.log(two_number)
+//             if(y.number.indexOf(this.result[i])>-1){
+//               seconed++;
+//               this.lucky_number.push({
+//               "number":this.result[i],
+//               "seconed":seconed
+//             })
+//             }
+//           })
+//         })
+//       }
+// console.log(this.lucky_number)
+//   }
 
 }
 
